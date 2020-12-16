a6f03bc9e2d9f99486bf26581b49a2e192631155
Tim Daubenschütz <tim@daubenschuetz.de>
2020-12-16 16:00:04 +0100

    Switch to worker_threads for inline modules

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // @format
-const { writeFileSync, unlinkSync, existsSync } = require("fs");
-const { fork } = require("child_process");
-const uuidv4 = require("uuid").v4;
+const { Worker } = require("worker_threads");
 
 const { once } = require("events");
 const express = require("express");
 
 const template = (fn, defaultCount) => `
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
+const { parentPort } = require('worker_threads');
+
 const app = express();
 let count = 0;
 
@@ -26,32 +26,21 @@ app.use(function(req, res, next) {
 ${fn}
 let server = app.listen(0, function () {
   const port = server.address().port;
-  process.send("PORT:"+port);
+  parentPort.postMessage("PORT:"+port);
 })
 `;
 
 async function createWorker(fn, defaultCount = 1) {
-  const fileName = `.${uuidv4()}`;
-  let worker = {};
+  const entry = template(fn, defaultCount);
+  const worker = new Worker(entry, {eval: true});
 
-  const file = template(fn, defaultCount);
-  writeFileSync(fileName, file);
-
-  const child = fork(fileName, {
-    cwd: process.cwd(),
-    detached: true,
-    stdio: [null, null, null, "ipc"]
-  });
-
-  const [data, _] = await once(child, "message");
+  const [data, _] = await once(worker, "message");
   const port = data.match(new RegExp("PORT:(\\d+)"))[1];
-  child.unref();
-  existsSync(fileName) && unlinkSync(fileName);
+  worker.unref();
 
   return {
-    process: child,
+    process: worker,
     port,
-    fileName
   };
 }
 
